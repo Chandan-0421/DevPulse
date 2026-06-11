fa13d9f07e80d81f5a491e18ef26282b5310f8ca
Chandan <chandansingh08521@gmail.com>
2026-06-11 16:42:08 +0530

    fix callback URL from env variable

--- backend/config/github.js
+++ backend/config/github.js
@@ -1,6 +1,6 @@
 module.exports = {
   clientId: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  redirectUri: 'http://localhost:5000/auth/callback',
+  redirectUri: process.env.CALLBACK_URL || 'http://localhost:5000/auth/callback',
   scope: 'read:user repo',
 }
